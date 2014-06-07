6099cf39c08c33ef6e239e86ef59f014bcf65c82
LilaHelminiak <lila.helminiak@gmail.com>
2014-06-08 00:44:18 +0200

    Subscriber&Publisher

---
+++ SubscribePublishWCF/serviceHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+
+namespace Microsoft.ServiceModel.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(SampleService)))
+            {
+                host.Open();
+
+                Console.WriteLine("The service is ready at: {0}", host.BaseAddresses.FirstOrDefault().ToString());
+                Console.WriteLine("Press <Enter> to stop the service");
+                Console.ReadKey();
+                host.Close();
+            }
+        }
+    }
+}
